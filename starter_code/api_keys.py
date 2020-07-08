eaa1b03fb8aaf9dadb6b087f3424558a53b47cb3
jay.sueno <jay.sueno@gmail.com>
2020-07-07 22:58:54 -0700

    working on pulling weather data with for loop, figuring out the try/except construct

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "14c639666d1f63e998fe531b1d81cfb4"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyDlGkSnKyUdsVNkQ5fIqp1Cf3E6pwWlZT4"
